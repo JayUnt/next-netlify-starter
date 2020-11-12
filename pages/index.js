40684b0d14f45ad64d3d35248c596fb95c474d4b
Jason Unterman <junterman@definedlogic.com>
2020-11-12 09:28:26 -0500

    add contact

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link';
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
@@ -6,14 +7,17 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Next.js Farter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my demo!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by clicking our conact page:
+          <Link href="/contact">
+            <a>Click here!!!</a>
+          </Link>
         </p>
       </main>
 
